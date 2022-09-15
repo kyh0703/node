2ec3b5c76a6b06ab6c5586da912eb5412ac967b1
Kimyeonho <kyh0703@nate.com>
2022-09-15 17:27:26 +0900

    [Feature] Exporess - Init1
    1. 자주 사용하는 미들웨어
    ya morgan cookie-arser express-session dotenv
    
    morgan: log tracer
    cookie-parser: cookie를 해석해 json으로
    body-parser: body data parse 기본으로 내장되어있음.
    express-session: session관리용 미들웨어

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import dotenv from 'dotenv';
 import path from 'path';
+
 const __dirname = path.resolve();
+
+dotenv.config();
 
 const app = express();
 app.set('port', process.env.PORT || 3005);
 
-app.use('/', (req, res, next) => {
-  console.log('all request');
+app.use(morgan('dev')); // log trace - dev, combined, common, short, tiny
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: 'session-cookie',
+  })
+);
+app.use((req, res, next) => {
+  console.log('모든 요청에 다 실행됩니다.');
   next();
 });
 
 app.get(
   '/',
   (req, res, next) => {
-    console.log('GET');
+    console.log('GET 요청에서만 실행됩니다.');
     next();
   },
   (req, res) => {
-    throw new Error('error go to the middleware');
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
   }
 );
 
@@ -25,6 +49,5 @@ app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send(err.message);
 });
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'));
-});
+
+app.listen(app.get('port'), () => {});
